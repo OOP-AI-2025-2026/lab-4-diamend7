6e902c03672374d49b386cc3e79f5435112c6bbf
diamend7 <108933026+diamend7@users.noreply.github.com>
2025-10-23 16:43:12 +0300

    Update DiscountBill.java

--- src/main/java/ua/opnu/bill/DiscountBill.java
+++ src/main/java/ua/opnu/bill/DiscountBill.java
@@ -1,50 +1,43 @@
 package ua.opnu.bill;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-
-
 public class DiscountBill extends GroceryBill {
 
-    private boolean regularCustomer;
-    private ArrayList<Item> itemList = getItems(); // изменил имя
+    private final boolean regularCustomer;
+    private int discountCount = 0;
+    private double discountAmount = 0.0;
 
     public DiscountBill(Employee clerk, boolean regularCustomer) {
         super(clerk);
         this.regularCustomer = regularCustomer;
     }
 
-    public double getTotal() {
-        double total;
-        if (!regularCustomer)
-            total = super.getTotal();
-        else
-            total = super.getTotal() - getDiscountAmount();
+    @Override
+    public void add(Item i) {
+        super.add(i);
+        if (regularCustomer && i.getDiscount() > 0) {
+            discountCount++;
+            discountAmount += i.getDiscount();
+        }
+    }
 
-        total = Math.round(total * 100.0) / 100.0;
-        return total;
+    @Override
+    public double getTotal() {
+        double full = super.getTotal();
+        return regularCustomer ? (full - discountAmount) : full;
     }
 
     public int getDiscountCount() {
-        int cnt = 0;
-        for (Item it : itemList)
-            if (it.getDiscount() > 0 && regularCustomer)
-                cnt++;
-        return cnt;
+        return regularCustomer ? discountCount : 0;
     }
 
     public double getDiscountAmount() {
-        double amt = 0;
-        for (Item it : itemList)
-            if (it.getDiscount() > 0 && regularCustomer)
-                amt += it.getDiscount();
-        return amt;
+        return regularCustomer ? discountAmount : 0.0;
     }
 
     public double getDiscountPercent() {
-        double discount = 100 - ((getTotal() * 100) / super.getTotal());
-        BigDecimal bd = new BigDecimal(Double.toString(discount));
-        bd = bd.setScale(13, BigDecimal.ROUND_HALF_UP);
-        return bd.doubleValue();
+        double full = super.getTotal();
+        if (!regularCustomer || full == 0) return 0.0;
+        double withDiscount = full - discountAmount;
+        return 100.0 - (withDiscount * 100.0) / full;
     }
 }
