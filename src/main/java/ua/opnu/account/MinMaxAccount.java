a713ec8f1dacbc5c45b2030b1f8cfd7fdf0ee28b
diamend7 <108933026+diamend7@users.noreply.github.com>
2025-10-23 16:44:24 +0300

    Update MinMaxAccount.java

--- src/main/java/ua/opnu/account/MinMaxAccount.java
+++ src/main/java/ua/opnu/account/MinMaxAccount.java
@@ -2,36 +2,40 @@ package ua.opnu.account;
 
 public class MinMaxAccount extends BankingAccount {
 
-  private int min;
-  private int max;
+    private int min;
+    private int max;
 
-  public MinMaxAccount(Startup s) {
-    super(s);
-    this.min = getBalance();
-    this.max = getBalance();
-  }
-
-  public int getMin() {
-    return min;
-  }
-
-  public int getMax() {
-    return max;
-  }
-
-  @Override
-  public void debit(Debit d) {
-    super.debit(d);
-    if (getBalance() > max) {
-      this.max = getBalance();
+    public MinMaxAccount(Startup s) {
+        super(s);
+        int balance = getBalance();  // баланс після super()
+        this.min = balance;
+        this.max = balance;
     }
-  }
 
-  @Override
-  public void credit(Credit c) {
-    super.credit(c);
-    if (getBalance() < min) {
-      this.min = getBalance();
+    @Override
+    public void credit(int cents) {
+        super.credit(cents);
+        updateMinMax();
     }
-  }
+
+    @Override
+    public boolean debit(int cents) {
+        boolean result = super.debit(cents);
+        updateMinMax();
+        return result;
+    }
+
+    private void updateMinMax() {
+        int balance = getBalance();
+        if (balance < min) min = balance;
+        if (balance > max) max = balance;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public int getMax() {
+        return max;
+    }
 }
